796b67a63e207a29f32d04234c37a4bec9caee28
jibarra436 <60481786+jibarra436@users.noreply.github.com>
2021-12-04 15:31:49 -0600

    Add JUnit 4 and CalculatorTest.java skeleton

---
+++ src/test/java/simplejavacalculator/CalculatorTest.java
@@ -0,0 +1,34 @@
+package simplejavacalculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testSciConversion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateBi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateEqual() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReset() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateMono() {
+		fail("Not yet implemented");
+	}
+
+}
